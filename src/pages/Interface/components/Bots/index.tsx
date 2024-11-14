54cfe4754cf6a3db2a04a628c303d158450ebf7a
gabrielcampoos <gabrielcampoos@hotmail.com>
2024-11-14 20:39:46 -0300

    commit

--- src/pages/Interface/components/Bots/index.tsx
+++ src/pages/Interface/components/Bots/index.tsx
@@ -17,115 +17,28 @@ export const Bots = () => {
 	const { files } = useAppSelector((state) => state.file);
 	const userState = useAppSelector((state) => state.user);
 	const dispatch = useAppDispatch();
-	const [loading, setLoading] = useState(false); // Estado de carregamento do download
-	const [uploading, setUploading] = useState(false); // Estado de carregamento do upload
-	const [selectedFile, setSelectedFile] = useState<File | null>(null); // Arquivo selecionado para upload
 	const [downloadLoading, setDownloadLoading] = useState<string | null>(null); // Agora aceita string ou null
 
 	useEffect(() => {
 		dispatch(listFiles());
 	}, [dispatch]);
 
-	// Função para lidar com a seleção de arquivos
-	const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-		if (event.target.files) {
-			const file = event.target.files[0]; // Assume que o usuário seleciona um único arquivo .rar
-
-			// Verifica se o arquivo tem a extensão .rar
-			if (file.name.endsWith('.rar')) {
-				setSelectedFile(file);
-			} else {
-				alert('Selecione um arquivo .rar');
-			}
-		}
-	};
-
-	// Função para fazer o upload
-	const handleUpload = async () => {
-		if (!selectedFile) {
-			alert('Selecione um arquivo .rar para upload.');
-			return;
-		}
-
-		setUploading(true);
-
-		const formData = new FormData();
-		formData.append('files', selectedFile); // Envia o arquivo .rar
-
-		try {
-			const token = localStorage.getItem('userLogged');
-			if (!token) {
-				console.error('Token de autenticação não encontrado');
-				setUploading(false);
-				return;
-			}
-
-			// Envia o arquivo .rar para o backend
-			const response = await fetch(
-				'https://panel-api-k76f.onrender.com/upload',
-				{
-					method: 'POST',
-					headers: {
-						Authorization: `Bearer ${token}`,
-					},
-					body: formData,
-				},
-			);
-
-			if (!response.ok) {
-				throw new Error('Erro ao fazer upload dos arquivos');
-			}
-
-			// Sucesso no upload
-			alert('Arquivo carregado com sucesso!');
-			setSelectedFile(null); // Limpa o arquivo selecionado
-		} catch (error) {
-			console.error('Erro ao realizar o upload:', error);
-			alert('Erro ao realizar o upload');
-		} finally {
-			setUploading(false); // Finaliza o carregamento
-		}
-	};
-
 	// Função para fazer o download
 	const handleDownload = async (fileId: string) => {
 		try {
 			setDownloadLoading(fileId); // Define o estado de carregamento para o arquivo em questão
 
-			// URL do backend definida na variável de ambiente
-			const fileUrl = `${import.meta.env.VITE_API_URL}/file/download-jonbet`;
+			// URL de download direta do Google Drive
+			const fileUrl = `https://drive.google.com/uc?export=download&id=${fileId}`;
 
-			const token = localStorage.getItem('userLogged');
+			// Criar o link de download
+			const link = document.createElement('a');
+			link.href = fileUrl;
+			link.download = 'arquivo.rar'; // Defina o nome do arquivo para o download
 
-			if (!token) {
-				console.error('Token de autenticação não encontrado');
-				setDownloadLoading(null); // Reseta o estado de carregamento
-				return;
-			}
-
-			// Requisição para o backend
-			const response = await fetch(fileUrl, {
-				method: 'GET',
-				headers: {
-					Authorization: `Bearer ${token}`,
-				},
-			});
-
-			if (response.ok) {
-				const blob = await response.blob();
-				const link = document.createElement('a');
-				link.href = URL.createObjectURL(blob);
-
-				// Ajuste para garantir que o nome do arquivo será 'JonBet.zip'
-				link.download = 'JonBet.zip'; // Aqui definimos o nome correto do arquivo
-
-				document.body.appendChild(link);
-				link.click();
-				URL.revokeObjectURL(link.href);
-				document.body.removeChild(link);
-			} else {
-				console.error('Erro ao baixar o arquivo:', response.status);
-			}
+			document.body.appendChild(link);
+			link.click();
+			document.body.removeChild(link);
 		} catch (error) {
 			console.error('Erro ao realizar o download:', error);
 		} finally {
@@ -135,44 +48,6 @@ export const Bots = () => {
 
 	return (
 		<Box sx={{ padding: 2 }}>
-			{/* Formulário de Upload */}
-			<Box sx={{ marginBottom: 2 }}>
-				{/* Usando um <input> para o upload de arquivos */}
-				<input
-					type="file"
-					onChange={handleFileChange}
-					style={{ display: 'none' }} // Ocultar o campo de input
-					id="file-upload"
-				/>
-
-				<label htmlFor="file-upload">
-					<Button
-						component="span"
-						variant="contained"
-						disabled={uploading}
-					>
-						{uploading ? (
-							<CircularProgress size={24} />
-						) : (
-							'Selecionar Arquivo .rar'
-						)}
-					</Button>
-				</label>
-
-				<Button
-					onClick={handleUpload}
-					disabled={uploading || !selectedFile}
-					variant="contained"
-					sx={{ marginTop: 2 }}
-				>
-					{uploading ? (
-						<CircularProgress size={24} />
-					) : (
-						'Fazer Upload'
-					)}
-				</Button>
-			</Box>
-
 			{/* Lista de arquivos */}
 			<Grid container spacing={2}>
 				{Array.isArray(files) && files.length > 0 ? (
@@ -192,14 +67,19 @@ export const Bots = () => {
 										disabled={
 											userState.active === false ||
 											downloadLoading !== null
-										} // Desativa o botão durante o download
+										}
 										size="small"
 										variant="contained"
-										onClick={() => handleDownload(file.id)} // Passa o id do arquivo para o handleDownload
+										onClick={() =>
+											handleDownload(
+												'12Ntlya_17AyxQmZ0gUTWkArdMrG3f6je',
+											)
+										} // Passa o ID do arquivo
 									>
-										{downloadLoading === file.id
+										{downloadLoading ===
+										'12Ntlya_17AyxQmZ0gUTWkArdMrG3f6je'
 											? 'Baixando...'
-											: 'Download'}{' '}
+											: 'Download'}
 									</Button>
 								</CardActions>
 							</Card>
